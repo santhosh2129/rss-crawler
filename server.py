602ee25b4c052b9d2cf928e64cbe616160779957
Santhosh Thiyagarajan <sthiyagarajan@owler.com>
2017-09-04 19:43:41 +0530

    changes

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ Created on Thu Aug 31 17:15:38 2017
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 import feedParser
+import pika
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +19,17 @@ parser.add_argument('url')
     
 def handleException():
     abort(404, message = "Unable to Parse RSS")
+
+def postInQueue(message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='feed_crawler')
+    channel.basic_publish(exchange='',
+                      routing_key='feed_crawler',
+                      body=json.dumps(message),
+                      properties=pika.BasicProperties(
+                     delivery_mode = 2))
+    connection.close()
 
 class FeedParser(Resource):
     def post (self):
@@ -39,6 +52,7 @@ class FeedParser(Resource):
             postMap['author'] = post.author
             postMap['summary'] = post.summary
             entriesList.append(postMap)
+            postInQueue(postMap)
         result['feeds'] = entriesList
         return entriesList
 
